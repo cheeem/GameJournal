5a2fc128454bce9ba788d7a89434e9fd31dc3e21
Jackson Werner <jacksontwerner@gmail.com>
2026-09-10 02:26:12 -0500

    update(content)

--- content.js
+++ content.js
@@ -1,4 +1,16 @@
-const content = [`# Hello World
-Hello World  
-Hello
+const content = [`# Prompt 1
+Week 1  
+September 9, 2026  
+  
+I chose Earthbound Beginnings from the classic games list.  
+  
+1. Was there a call-to-action that persuaded you to continue playing the game? 
+2. How did the game teach you its main mechanics?
+3. How did it guide you?
+4. Did you get lost or confused?
+5. Did the game introduce an emotional element at the beginning to motivate the player’s journey?
+6. Can you predict your overall goal in the game from these first 5 minutes?
+7. Explain how you built a mental model of the game as you started playing.
+8. How does the game feel? Do the actions feel responsive? Does movement always feel controllable or do you feel like you might lose precision over character movement in some situations? Etc.
+9. How do the game visuals and audio influence the gameplay experience?
 `,];
