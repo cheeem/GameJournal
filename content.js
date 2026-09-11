e054fdcbbb17b7678a2a89c851a25e05e3a54814
Jackson Werner <jacksontwerner@gmail.com>
2026-09-10 22:51:05 -0500

    update(content)

--- content.js
+++ content.js
@@ -1,16 +1,93 @@
-const content = [`## Prompt 1
+const content = [`# Prompt 1
 Week 1  
 September 9, 2026  
-### Chosen Game
+<br>
 I chose Earthbound Beginnings from the classic games list.  
-### First Impressions
-1. Was there a call-to-action that persuaded you to continue playing the game? 
-2. How did the game teach you its main mechanics?
-3. How did it guide you?
-4. Did you get lost or confused?
-5. Did the game introduce an emotional element at the beginning to motivate the player’s journey?
-6. Can you predict your overall goal in the game from these first 5 minutes?
-7. Explain how you built a mental model of the game as you started playing.
-8. How does the game feel? Do the actions feel responsive? Does movement always feel controllable or do you feel like you might lose precision over character movement in some situations? Etc.
-9. How do the game visuals and audio influence the gameplay experience?
+<br>
+1. **Was there a call-to-action that persuaded you to continue playing the
+   game?**  
+    Not really, the game intially prompted me with some names and my favorite
+    food, and then dropped me into its world where I ended up running into
+    enemies and discovering new things about my world. It lacked instruction or 
+    a tutorial, instead relying on visual cues and curiosity to keep me engaged.
+      
+    Everything very spontaneous. Through unexpected interactions like being
+    attacked by a lamp when I tried to interact with a door to finding a music
+    box in a doll I just defeated in battle to hearing about possessed spirits
+    and my grandfather's research, I was made curious and I ended up playing the
+    game longer than I expected.  
+    <br>
+2. **How did the game teach you its main mechanics?**  
+    Through that spontaneous duel against a lamp, I was taught part of the
+    combat system, where I would have to decide what action to take during each
+    "turn" of the fight. I didn't understand every option available to me, but
+    the menu was simplistic enough that I could stumble my way through a fight
+    and learn the turn-based nature of the combat in the game. Through
+    interacting with a character, I learned that my grandfather apparently knows
+    something about that PSI option in the combat and interaction menus. Curious
+    what that meant, I selected that option and saw a "telepathy" option.  
+    The details of the game's world led me to attempt to understand more deeply 
+    what options I have as a player, rooted in curiosity rather than confusion 
+    when not understanding how to proceed in a situation or on a level.  
+    <br>
+3. **How did it guide you?**  
+    Instead of guiding me through the world and instructing me, the game seemed 
+    to intentially make me confused and weirdly stressed with flashing orange 
+    lighting and intense music.  
+    While I immediately drew visual comparisions to a game like Undertale,
+    something that also has you explore its world and uncover things, I remember
+    Undertale's world providing much more guidance and context initially.  
+    <br>
+4. **Did you get lost or confused?**  
+    I was definitely confused, and most things I saw ended up leaving me with
+    more questions, but I never felt lost. The game quickly helped me understand
+    how I could interact with its world and made it obvious what I could explore
+    to learn more.  
+    <br>
+5. **Did the game introduce an emotional element at the beginning to motivate 
+the player’s journey?**  
+    With immediatelly putting me under attack by various objections meant to be
+    inanimate, and then through the flashing lights and tense music I mentioned
+    before, it seems to play off of anxiety more than anything to push me to
+    explore the world.  
+    <br>
+6. **Can you predict your overall goal in the game from these first 5 minutes?**
+  
+    I genuinely have no idea. I assume it has something to do with the parental
+    figures in the game, since the game opens with talking about people from the
+    far past, has characters mentioning my father and grandfather, and the game
+    is also known as "Mother". I can also assume I'll have to engage in more
+    combat, pick up more skills since I can't perform telepathy when I try, and
+    perhaps gain those skills through leveling up which happened after I
+    defeated the doll. The final goal is unclear, but I'm definitely curious and
+    that was enough to keep me going. It feels like that end goal is something
+    I'm going to have to uncover, just like I discovered that PSI is tied to
+    special abilities like telepathy.  
+    <br> 
+7. **Explain how you built a mental model of the game as you started playing.**
+    Again, everything I discovered and understood about the game, its story, and
+    its mechanics came from things spotaneously happening as I sought out visual
+    cues for how to proceed. It started with the combat system where I learned
+    how to "guard" and fight, and contiued when I started to interact with
+    characters and saw new menus that enabled me to talk. 
+    <br>
+8. **How does the game feel? Do the actions feel responsive? Does movement 
+always feel controllable or do you feel like you might lose precision over 
+character movement in some situations? Etc.**  
+    The actions and especially the menus felt very quick and responsive. The
+    delays and turn-based nature of combat made those interactions more of a
+    vague puzzle to solve rather than something I would need to quickly respond
+    to. The movement felt a bit like I was skating on ice at first, but I got
+    the hang of it pretty quickly.
+    <br>
+9. **How do the game visuals and audio influence the gameplay experience?**
+    The visuals and the audio were the main things keeping me progressing in the
+    game. As I described before, they made me a combination of stressed and
+    curious in the opening scene of the game. In the opening menus and
+    after I defeated the doll, the music felt very welcoming and calming in
+    comparision to that opening scene, building tension due to the contrast
+    between the tense and calm music. The visuals acted as a great guide for
+    helping me think of what to do next. Placing things like doors showed me
+    where I could go, and by keeping rooms sparely decorated, it quickly became
+    clear what I could and should interact with in those rooms.
 `,];
